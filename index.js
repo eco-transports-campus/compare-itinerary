d41868cd7adb4c6e5422eadf3fba77305b8f145b
busquets <alexis.pointurier@gmail.com>
2018-05-02 11:01:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function getDistanceFromLatLonInKm(p1 ,p2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(p2["lat"]-p1["lat"]);  // deg2rad below
+  var dLon = deg2rad(p2["lon"]-p1["lon"]); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(p1["lat"])) * Math.cos(deg2rad(p2["lat"])) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+exports.getSameTraject = function(itinerary, data, r) {
+	var trajects = [];
+	for(var i = 0; i < data.length; i++){
+		if (getDistanceFromLatLonInKm(itinerary["from"], data[i]["from"]) < r && getDistanceFromLatLonInKm(itinerary["to"], data[i]["to"]) < r){
+			trajects.push(data[i]);
+		}
+	}
+	return trajects;	
+};
